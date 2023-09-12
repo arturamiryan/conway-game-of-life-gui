a803edfd1d33e6ef5e5aeca3c01015dbbd526bbd
Artur Amiryan <arturamiryan0x@gmail.com>
2023-09-12 16:01:57 +0200

    Start developing GUI and some refactoring

---
+++ src/cells.rs
@@ -0,0 +1,158 @@
+use crate::{HEIGHT, WIDTH};
+
+#[derive(Clone, Copy)]
+struct Cell {
+    alive: bool,
+    color: [u8; 4],
+}
+
+impl Cell {
+    pub fn is_alive(self) -> bool {
+        self.alive
+    }
+
+    pub fn set_alive(&mut self) {
+        self.alive = true;
+    }
+
+    pub fn set_dead(&mut self) {
+        self.alive = false;
+    }
+
+    pub fn set_color(&mut self, color: [u8; 4]) {
+        self.color = color;
+    }
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Cell {
+            alive: false,
+            color: [0, 0xff, 0, 0xf0],
+        }
+    }
+}
+
+pub struct Grid {
+    cells: Vec<Vec<Cell>>,
+    swap_cells: Vec<Vec<Cell>>,
+    cells_color: [u8; 4],
+}
+
+impl Grid {
+    pub fn new(width: u32, height: u32, cells_color: [u8; 4]) -> Grid {
+        Self {
+            cells: vec![
+                vec![
+                    Cell {
+                        alive: false,
+                        ..Default::default()
+                    };
+                    width as usize
+                ];
+                height as usize
+            ],
+            swap_cells: vec![
+                vec![
+                    Cell {
+                        alive: false,
+                        ..Default::default()
+                    };
+                    width as usize
+                ];
+                height as usize
+            ],
+            cells_color,
+        }
+    }
+
+    pub fn set_cells_color(&mut self, cells_color: [u8; 4]) {
+        self.cells_color = cells_color;
+    }
+
+    pub fn get_mut_cells_color(&mut self) -> &mut [u8; 4] {
+        &mut self.cells_color
+    }
+
+    pub fn randomize(&mut self) {
+        for i in 0..HEIGHT as usize {
+            for j in 0..WIDTH as usize {
+                self.cells[i][j] = Cell {
+                    alive: rand::random(),
+                    color: self.cells_color,
+                };
+            }
+        }
+    }
+
+    pub fn set_blank(&mut self) {
+        for i in 0..HEIGHT as usize {
+            for j in 0..WIDTH as usize {
+                self.cells[i][j] = Cell {
+                    alive: false,
+                    color: self.cells_color,
+                };
+            }
+        }
+    }
+
+    pub fn draw(&self, screen: &mut [u8]) {
+        let mut pix = screen.chunks_exact_mut(4);
+        for row in self.cells.iter() {
+            for cell in row {
+                let color = if cell.is_alive() {
+                    cell.color
+                } else {
+                    [0, 0xff, 0x01, 0x05]
+                };
+                pix.next().unwrap().copy_from_slice(&color);
+            }
+        }
+    }
+
+    pub fn check_rules(&mut self) {
+        for i in 0..HEIGHT {
+            for j in 0..WIDTH {
+                self.swap_cells[i as usize][j as usize] = Cell {
+                    alive: self.decide(i, j),
+                    color: self.cells_color,
+                };
+            }
+        }
+        std::mem::swap(&mut self.swap_cells, &mut self.cells);
+    }
+
+    pub fn set_pixel_alive(&mut self, x: f32, y: f32) {
+        self.cells[y as usize][x as usize].set_alive();
+    }
+
+    pub fn set_pixel_dead(&mut self, x: f32, y: f32) {
+        self.cells[y as usize][x as usize].set_dead();
+    }
+
+    // Total neighbourgs of (i, j) cell
+    fn get_neighbours(&self, i: i32, j: i32) -> usize {
+        self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
+            .is_alive() as usize
+            + self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
+                .is_alive() as usize
+    }
+
+    fn decide(&self, i: i32, j: i32) -> bool {
+        let cells_alive = self.get_neighbours(i, j);
+
+        cells_alive == 3 || (cells_alive == 2 && self.cells[i as usize][j as usize].is_alive())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
+use crate::gui::Framework;
+use egui::Color32;
 use pixels::{Error, Pixels, SurfaceTexture};
 use std::{thread, time};
 use winit::{
@@ -10,6 +12,11 @@ use winit::{
     window::WindowBuilder,
 };
 use winit_input_helper::WinitInputHelper;
+
+mod cells;
+mod gui;
+
+use cells::*;
 
 const SCALE: f64 = 2.0;
 const WIDTH: i32 = (1920.0 / SCALE) as i32;
@@ -31,38 +38,29 @@ fn main() -> Result<(), Error> {
             .unwrap()
     };
 
-    let mut pixels = {
+    let (mut pixels, mut framework) = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?
+        let pixels = Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?;
+        let framework = Framework::new(
+            &event_loop,
+            window_size.width,
+            window_size.height,
+            SCALE as f32,
+            &pixels,
+        );
+        (pixels, framework)
     };
-    let mut grid = Grid::new(WIDTH as u32, HEIGHT as u32);
+    let mut grid = Grid::new(WIDTH as u32, HEIGHT as u32, [0, 0xff, 0, 0xf0]);
     grid.randomize();
     let mut stop = false;
     let mut speed = time::Duration::from_millis(0);
 
     event_loop.run(move |event, _, control_flow| {
-        // Window redraw
-        match event {
-            Event::RedrawEventsCleared => {
-                grid.draw(pixels.frame_mut());
-                if let Err(err) = pixels.render() {
-                    eprintln!("{}", err);
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-                if !stop {
-                    grid.check_rules();
-                    thread::sleep(speed);
-                }
-            }
-            _ => (),
-        }
-
         // Watch for keypresses
         if input.update(&event) {
             // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
@@ -101,116 +99,38 @@ fn main() -> Result<(), Error> {
             if input.mouse_pressed(1) {
                 grid.set_pixel_dead(mouse_x / SCALE as f32, mouse_y / SCALE as f32);
             }
+
+            window.request_redraw();
+        }
+        // Window redraw
+        match event {
+            Event::WindowEvent { event, .. } => {
+                // Update egui inputs
+                framework.handle_event(&event);
+            }
+            Event::RedrawEventsCleared => {
+                // Prepare egui
+                framework.prepare(&window);
+                grid.draw(pixels.frame_mut());
+                if let Err(err) = pixels.render_with(|encoder, render_target, context| {
+                    // Render the world texture
+                    context.scaling_renderer.render(encoder, render_target);
+
+                    // Render egui
+                    framework.render(encoder, render_target, context);
+
+                    Ok(())
+                }) {
+                    eprintln!("{}", err);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+                if !stop {
+                    grid.check_rules();
+                    thread::sleep(speed);
+                }
+            }
+            _ => (),
         }
     });
-}
-
-#[derive(Clone, Copy)]
-struct Cell {
-    alive: bool,
-}
-
-impl Cell {
-    pub fn is_alive(self) -> bool {
-        self.alive
-    }
-
-    pub fn set_alive(&mut self) {
-        self.alive = true;
-    }
-
-    pub fn set_dead(&mut self) {
-        self.alive = false;
-    }
-}
-
-struct Grid {
-    cells: Vec<Vec<Cell>>,
-    swap_cells: Vec<Vec<Cell>>,
-}
-
-impl Grid {
-    pub fn new(width: u32, height: u32) -> Grid {
-        Self {
-            cells: vec![vec![Cell { alive: false }; width as usize]; height as usize],
-            swap_cells: vec![vec![Cell { alive: false }; width as usize]; height as usize],
-        }
-    }
-
-    pub fn randomize(&mut self) {
-        for i in 0..HEIGHT as usize {
-            for j in 0..WIDTH as usize {
-                self.cells[i][j] = Cell {
-                    alive: rand::random(),
-                };
-            }
-        }
-    }
-
-    pub fn set_blank(&mut self) {
-        for i in 0..HEIGHT as usize {
-            for j in 0..WIDTH as usize {
-                self.cells[i][j] = Cell { alive: false };
-            }
-        }
-    }
-
-    pub fn draw(&self, screen: &mut [u8]) {
-        let mut pix = screen.chunks_exact_mut(4);
-        for row in self.cells.iter() {
-            for cell in row {
-                let color = if cell.is_alive() {
-                    [0, 0xff, 0, 0xf0]
-                } else {
-                    [0, 0xff, 0x01, 0x05]
-                };
-                pix.next().unwrap().copy_from_slice(&color);
-            }
-        }
-    }
-
-    pub fn check_rules(&mut self) {
-        for i in 0..HEIGHT {
-            for j in 0..WIDTH {
-                self.swap_cells[i as usize][j as usize] = Cell {
-                    alive: self.decide(i, j),
-                };
-            }
-        }
-        std::mem::swap(&mut self.swap_cells, &mut self.cells);
-    }
-
-    fn set_pixel_alive(&mut self, x: f32, y: f32) {
-        self.cells[y as usize][x as usize].set_alive();
-    }
-
-    fn set_pixel_dead(&mut self, x: f32, y: f32) {
-        self.cells[y as usize][x as usize].set_dead();
-    }
-
-    // Total neighbourgs of (i, j) cell
-    fn get_neighbours(&self, i: i32, j: i32) -> usize {
-        self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
-            .is_alive() as usize
-            + self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i - 1 + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j - 1 + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-            + self.cells[((i + 1 + HEIGHT) % HEIGHT) as usize][((j + 1 + WIDTH) % WIDTH) as usize]
-                .is_alive() as usize
-    }
-
-    fn decide(&self, i: i32, j: i32) -> bool {
-        let cells_alive = self.get_neighbours(i, j);
-
-        cells_alive == 3 || (cells_alive == 2 && self.cells[i as usize][j as usize].is_alive())
-    }
 }
